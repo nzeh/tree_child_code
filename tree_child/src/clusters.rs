eabfbc4cccc69dc88f3f58f105a62fbb4c8d0ea5
Norbert Zeh <nzeh@cs.dal.ca>
2018-05-21 21:31:30 -0300

    Finish LCA labelling for cluster partitioning

--- tree_child/src/clusters.rs
+++ tree_child/src/clusters.rs
@@ -1,23 +1,25 @@
-//! This module implements cluster partitioning for a collection of trees with the same label set
-//! `[0..n-1]`.
+//! This module implements cluster partitioning for a collection of trees.  This implementation
+//! does not work with the leaf labels directly to find matching leaves in different trees.
+//! Instead, it assumes that the leaf IDs, which are unique integers in [0,n-1] for each tree, are
+//! chosen so that the leaves with the same labels in different trees have the same integer ID.
 
 
-use std::mem;
-use tree::{Info, Node, Tree};
+use std::default::Default;
+use tree::{Node, NodeData, Tree};
 //use tree_child_sequence::{Pair, TcSeq};
 
 
-/// The type used to represent clusters in cluster partition
+/// The type used to represent clusters in the cluster partition
 #[derive(Clone, Copy)]
 pub struct Cluster(usize);
 
 
 /// Type used to distinguish between leaves that are original leaves and leaves that represent
 /// clusters produced by the cluster partition.
-pub enum Loc<T> {
+pub enum LoC<T> {
 
     /// An original leaf
-    Node(T),
+    Leaf(T),
 
     /// A leaf representing a cluster
     Cluster(Cluster),
@@ -27,26 +29,26 @@ pub enum Loc<T> {
 /// Partition the input trees into clusters.  In the output, the clusters are arranged bottom-up,
 /// that is, the cluster containing the leaf representing another cluster C follows C in the
 /// cluster sequence.
-pub fn partition<T>(trees: Vec<Tree<T>>) -> Vec<Vec<Tree<Loc<T>>>> {
+pub fn partition<T>(trees: Vec<Tree<T>>) -> Vec<Vec<Tree<LoC<T>>>> {
 
     // Compute an interval labelling for every tree and pair each tree with its labelling
-    let preorder_numbers = trees.iter().map(leaf_intervals);
-    let numbered_trees = trees.iter().zip(preorder_numbers).collect::<Vec<(&Tree<T>, Vec<(usize, usize)>)>>();
+    let intervals = trees.iter().map(leaf_intervals);
+    let numbered_trees = trees.iter().zip(intervals).collect::<Vec<(&Tree<T>, Vec<(usize, usize)>)>>();
 
     // Map nodes of the first tree to the LCAs of their descendant leaves in all other trees and
     // vice versa.
     let forward_maps = numbered_trees[1..].iter()
-        .map(|t| map_to_lca(&numbered_trees[0], t)).collect::<Vec<Vec<usize>>>();
+        .map(|t| map_to_lca(&numbered_trees[0], t)).collect::<Vec<Vec<Node>>>();
     let backward_maps = numbered_trees[1..].iter()
-        .map(|t| map_to_lca(t, &numbered_trees[0])).collect::<Vec<Vec<usize>>>();
+        .map(|t| map_to_lca(t, &numbered_trees[0])).collect::<Vec<Vec<Node>>>();
 
     // Decompose the trees into clusters
     decompose_trees(&trees, forward_maps, backward_maps)
 }
 
 
-/// Number the leaves in `t` left to right and label every node with the interval of numbers
-/// of its descendant leaves.
+/// Number the leaves in `tree` left to right and label every node with the interval of numbers of
+/// its descendant leaves.
 fn leaf_intervals<T>(tree: &Tree<T>) -> Vec<(usize, usize)> {
     LeafLabeller::new(tree).run()
 }
@@ -82,14 +84,17 @@ impl<'a, T: 'a> LeafLabeller<'a, T> {
     fn traverse(&mut self, node: Node) {
         match self.tree[node] {
             
-            Info::Leaf(_) => {
+            // The number of a leaf is the next available number
+            NodeData::Leaf(_) => {
                 self.intervals[node.id()] = (self.count, self.count);
                 self.count += 1;
             },
 
-            Info::Internal(ref info) => {
+            // The interval of an internal node starts at the number given to its first
+            // descendant child and ends at the number given to its last descendant child.
+            NodeData::Internal(ref data) => {
                 let start = self.count;
-                for child in self.tree.children(info) {
+                for child in self.tree.children(data) {
                     self.traverse(child);
                 }
                 self.intervals[node.id()] = (start, self.count - 1);
@@ -100,18 +105,104 @@ impl<'a, T: 'a> LeafLabeller<'a, T> {
 
 
 /// Map every node in tree t1 to the LCA of its descendant leaves in t2
-fn map_to_lca<T>(t1: &(&Tree<T>, Vec<(usize, usize)>), t2: &(&Tree<T>, Vec<(usize, usize)>)) -> Vec<usize> {
-    let mapping = vec![0; t1.0.node_count()];
-    // TODO: Finish this
-    mapping
+fn map_to_lca<T>(t1: &(&Tree<T>, Vec<(usize, usize)>), t2: &(&Tree<T>, Vec<(usize, usize)>)) -> Vec<Node> {
+    LcaMapper::new(t1, t2).run()
 }
+
+
+/// The status of the LCA mapping process
+struct LcaMapper<'a, T: 'a> {
+
+    /// The tree to be mapped
+    tree: &'a Tree<T>,
+
+    /// Mapping from nodes to their parents in the tree to be mapped to.  (Extracted from the tree
+    /// because the mapping process shortcuts these pointers to guarantee linear time.)
+    parents: Vec<Option<Node>>,
+
+    /// The leaf intervals associated with the nodes in the second tree.
+    intervals: &'a Vec<(usize, usize)>,
+
+    /// The constructed mapping
+    mapping: Vec<Node>,
+}
+
+
+impl<'a, T: 'a> LcaMapper<'a, T> {
+
+    /// Create a new `LcaMapper` for a pair of trees whose nodes have been numbered with leaf
+    /// intervals.
+    fn new(t1: &'a (&Tree<T>, Vec<(usize, usize)>), t2: &'a (&Tree<T>, Vec<(usize, usize)>)) -> LcaMapper<'a, T> {
+
+        // Set up the initial mapping between matching leaves
+        let mut mapping = vec![Default::default(); t1.0.node_count()];
+        for leaf in t1.0.leaves() {
+            mapping[t1.0[leaf.id()].id()] = t2.0[leaf.id()];
+        }
+
+        // Return the initial mapper state
+        LcaMapper {
+            tree:      t1.0,
+            parents:   t2.0.nodes().map(|node| node.parent()).collect::<Vec<Option<Node>>>(),
+            intervals: &t2.1,
+            mapping,
+        }
+    }
+
+    /// Run the LCA mapper
+    fn run(mut self) -> Vec<Node> {
+        self.traverse(self.tree.root().unwrap());
+        self.mapping
+    }
+
+    /// Traverse the subtree of `self.t1` rooted in the given node and map all nodes in the subtree
+    /// to corresponding nodes in `self.t2`.
+    fn traverse(&mut self, node: Node) -> Node {
+
+        // Leaves are already mapped, so deal only with internal nodes
+        if let NodeData::Internal(ref data) = self.tree[node] {
+
+            // Get the first candidate image as the image computed for the first child
+            let mut children = self.tree.children(data);
+            let first_buddy = self.traverse(children.next().unwrap());
+
+            // Move the image up the tree to encompass the images of all the other children
+            let mut buddy = first_buddy;
+            for child in children {
+                let interval = self.intervals[self.traverse(child).id()];
+                while !contains(self.intervals[buddy.id()], interval) {
+                    buddy = self.parents[buddy.id()].unwrap();
+                }
+            }
+
+            // Shortcut the parent pointers on the traversed path so subsequent searches that hit
+            // this path jump right up to its top
+            let mut on_path = first_buddy;
+            while on_path != buddy {
+                self.parents[on_path.id()] = Some(buddy);
+            }
+
+            // Store the computed image of the current node
+            self.mapping[node.id()] = buddy;
+        }
+
+        self.mapping[node.id()]
+    }
+}
+
+
+/// Checks whether one interval contains another
+fn contains(i1: (usize, usize), i2: (usize, usize)) -> bool {
+    i1.0 <= i2.0 && i1.1 >= i2.1
+}
+
 
 /// Decompose the trees into clusters based on LCA mappings between the first tree and the other
 /// trees
 fn decompose_trees<T>(
     trees:         &Vec<Tree<T>>,
-    forward_maps:  Vec<Vec<usize>>,
-    backward_maps: Vec<Vec<usize>>) -> Vec<Vec<Tree<Loc<T>>>> {
+    forward_maps:  Vec<Vec<Node>>,
+    backward_maps: Vec<Vec<Node>>) -> Vec<Vec<Tree<LoC<T>>>> {
     vec![]
 }
 
--- tree_child/src/tree.rs
+++ tree_child/src/tree.rs
@@ -1,121 +1,156 @@
-extern crate slab;
-
-use std::cell::RefCell;
 use std::ops::Index;
-use std::rc::Rc;
+use std::slice;
 
-/// A node pool to be shared by multiple trees
-pub struct NodePool {
-    pool: Rc<RefCell<slab::Slab<Info>>>,
-}
 
 /// A phylogenetic tree whose leaves have `usize` labels
 pub struct Tree<T> {
+
     /// The set of nodes in the tree
-    nodes: slab::Slab<Info>,
+    nodes: Vec<NodeData<T>>,
+
+    /// The root of the tree
+    root: Option<Node>,
 
     /// The set of leaves in the tree
     leaves: Vec<Node>,
 
-    /// The leaf labels
-    labels: Vec<T>,
-
-    /// The root of the tree
-    root: Option<Node>,
+    /// Number of nodes in this tree
+    node_count: usize,
 }
 
+
 impl<T> Tree<T> {
-    /// The number of nodes in this tree
-    pub fn node_count(&self) -> usize {
-        self.nodes.len()
+
+    /// Create a new tree
+    pub fn new() -> Tree<T> {
+        Tree {
+            nodes:      vec![],
+            root:       None,
+            leaves:     vec![],
+            node_count: 0,
+        }
     }
 
-    /// The root node of this tree
+    /// The number of nodes in the tree
+    pub fn node_count(&self) -> usize {
+        self.node_count
+    }
+
+    /// The root node of the tree
     pub fn root(&self) -> Option<Node> {
         self.root
     }
 
-    /// Iterator over the children of a node
-    pub fn children(&self, info: &InternalInfo) -> ChildIter<T> {
-        ChildIter {
+    /// Iterator over the nodes of the tree
+    pub fn nodes(&self) -> NodeIter<T> {
+        NodeIter { iter: self.nodes.iter() }
+    }
+
+    /// Iterator over the leaves of the tree
+    pub fn leaves(&self) -> LeafIter<T> {
+        LeafIter {
             tree: self,
-            child: Some(info.first_child),
+            iter: self.leaves.iter(),
+        }
+    }
+
+    /// Iterator over the children of a node
+    pub fn children(&self, node: &InternalNodeData) -> ChildIter<T> {
+        ChildIter {
+            tree:  self,
+            child: Some(node.first_child),
         }
     }
 }
 
+
 /// The type used to represent tree nodes
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default, Eq, PartialEq)]
 pub struct Node(usize);
 
+
 impl Node {
+
     /// Access the ID of this node
-    pub fn id(&self) -> usize {
+    pub fn id(self) -> usize {
         self.0
     }
 }
 
-/// Indexing into a tree using a Node returns the corresponding node info
+
+/// Indexing into a tree using a `Node` returns the corresponding node data
 impl<T> Index<Node> for Tree<T> {
-    type Output = Info;
-    fn index(&self, index: Node) -> &Info {
+
+    type Output = NodeData<T>;
+
+    fn index(&self, index: Node) -> &NodeData<T> {
         &self.nodes[index.id()]
     }
 }
 
-/// The type used to represent tree leaves
-#[derive(Clone, Copy)]
+
+#[derive(Clone, Copy, Default, Eq, PartialEq)]
 pub struct Leaf(usize);
 
+
 impl Leaf {
-    /// Access the ID of this node
-    pub fn id(&self) -> usize {
+
+    /// Access the ID of this leaf
+    pub fn id(self) -> usize {
         self.0
     }
 }
 
-/// Indexing into a tree using a Leaf returns the corresponding node
+
+/// Indexing into a tree using a `Leaf` returns the corresponding `Node`
 impl<T> Index<Leaf> for Tree<T> {
+
     type Output = Node;
+
     fn index(&self, index: Leaf) -> &Node {
         &self.leaves[index.id()]
     }
 }
 
-/// The info associated with a node
-pub enum Info {
-    Leaf(LeafInfo),
-    Internal(InternalInfo),
+
+/// The data associated with a node
+pub enum NodeData<T> {
+    Leaf(LeafData<T>),
+    Internal(InternalNodeData),
 }
 
-impl Info {
+
+impl<T> NodeData<T> {
+
     /// Access the parent of the current node
     pub fn parent(&self) -> Option<Node> {
         match self {
-            Info::Leaf(info) => info.parent(),
-            Info::Internal(info) => info.parent(),
+            NodeData::Leaf(info)     => info.parent(),
+            NodeData::Internal(info) => info.parent(),
         }
     }
 
     /// Access the left sibling of the current node
     pub fn left(&self) -> Option<Node> {
         match self {
-            Info::Leaf(info) => info.left(),
-            Info::Internal(info) => info.left(),
+            NodeData::Leaf(info)     => info.left(),
+            NodeData::Internal(info) => info.left(),
         }
     }
 
     /// Access the right sibling of the current node
     pub fn right(&self) -> Option<Node> {
         match self {
-            Info::Leaf(info) => info.right(),
-            Info::Internal(info) => info.right(),
+            NodeData::Leaf(info)     => info.right(),
+            NodeData::Internal(info) => info.right(),
         }
     }
 }
 
+
 /// The info associated with a leaf
-pub struct LeafInfo {
+pub struct LeafData<T> {
+
     /// Parent
     parent: Option<Node>,
 
@@ -125,11 +160,15 @@ pub struct LeafInfo {
     /// Right sibling
     right: Option<Node>,
 
+    /// The leaf's label
+    label: T,
+
     /// The leaf's ID
     id: Leaf,
 }
 
-impl LeafInfo {
+impl<T> LeafData<T> {
+
     /// Access the parent of this leaf
     pub fn parent(&self) -> Option<Node> {
         self.parent
@@ -144,10 +183,17 @@ impl LeafInfo {
     pub fn right(&self) -> Option<Node> {
         self.right
     }
+
+    /// Access the leaf's ID
+    pub fn id(&self) -> Leaf {
+        self.id
+    }
 }
 
+
 /// The info associated with an internal node
-pub struct InternalInfo {
+pub struct InternalNodeData {
+
     /// Parent
     parent: Option<Node>,
 
@@ -161,7 +207,9 @@ pub struct InternalInfo {
     first_child: Node,
 }
 
-impl InternalInfo {
+
+impl InternalNodeData {
+
     /// Access the parent of this internal node
     pub fn parent(&self) -> Option<Node> {
         self.parent
@@ -178,238 +226,63 @@ impl InternalInfo {
     }
 }
 
-/// An iterator over the children of a node
-pub struct ChildIter<'a, T: 'a> {
-    tree: &'a Tree<T>,
-    child: Option<Node>,
+
+/// An iterator over the nodes of a tree
+pub struct NodeIter<'a, T: 'a> {
+    iter: slice::Iter<'a, NodeData<T>>,
 }
 
-impl<'a, T: 'a> Iterator for ChildIter<'a, T> {
-    type Item = Node;
-    fn next(&mut self) -> Option<Node> {
-        match self.child {
+
+impl<'a, T: 'a> Iterator for NodeIter<'a, T> {
+
+    type Item = &'a NodeData<T>;
+
+    fn next(&mut self) -> Option<&'a NodeData<T>> {
+        self.iter.next()
+    }
+}
+
+
+/// An iterator over the leaves of a tree
+pub struct LeafIter<'a, T: 'a> {
+    tree: &'a Tree<T>,
+    iter: slice::Iter<'a, Node>,
+}
+
+
+impl <'a, T: 'a> Iterator for LeafIter<'a, T> {
+
+    type Item = &'a LeafData<T>;
+
+    fn next(&mut self) -> Option<&'a LeafData<T>> {
+        match self.iter.next() {
             None => None,
-            Some(node) => {
-                self.child = self.tree[node].right();
-                Some(node)
+            Some(&leaf) => {
+                match self.tree[leaf] {
+                    NodeData::Leaf(ref data) => Some(data),
+                    _                        => None,
+                }
             }
         }
     }
 }
 
-//impl Tree {
-//
-//    /// Create a new tree
-//    pub fn new() -> Tree {
-//        Tree {
-//            nodes: slab::Slab::new(),
-//            root:  None,
-//        }
-//    }
-//
-//    /// Create a new leaf with the given label
-//    fn new_leaf(&mut self, label: usize) -> Node {
-//        Node(self.nodes.insert(
-//                NodeRep::new(NodeData::Leaf(label))))
-//    }
-//
-//    /// Create a new internal node with the given list of children
-//    fn new_internal(&mut self, children: Vec<Node>) -> Node {
-//
-//        let mut iter = children.into_iter();
-//        let mut last = iter.next().unwrap();
-//
-//        let parent = Node(self.nodes.insert(
-//                NodeRep::new(NodeData::Internal(last))));
-//
-//        {
-//            let last = self.get_mut(last);
-//            last.left   = None;
-//            last.parent = Some(parent);
-//        }
-//
-//        for child in iter {
-//            {
-//                let child = self.get_mut(child);
-//                child.left   = Some(last);
-//                child.parent = Some(parent);
-//            }
-//            self.get_mut(last).right = Some(child);
-//            last = child;
-//        }
-//        self.get_mut(last).right = None;
-//
-//        parent
-//    }
-//
-//    /// Make the given node the root of the tree
-//    fn set_root(&mut self, root: Node) {
-//        self.root = Some(root)
-//    }
-//
-//    /// Get a shared reference to a node from its ID
-//    fn get(&self, node: Node) -> &NodeRep {
-//        &self.nodes[node.0]
-//    }
-//
-//    /// Get a mutable reference to a node from its ID
-//    fn get_mut(&mut self, node: Node) -> &mut NodeRep {
-//        &mut self.nodes[node.0]
-//    }
-//
-////     /// Prune the subtree rooted in the given node from the tree
-////     pub fn prune(&mut self, node: Node) -> TreeOp {
-////         let (parent, remop) = self.remove_from_parent(node);
-////         match parent {
-////             Some(p) => {
-////                 match self.try_suppress(p) {
-////                     Some(suppop) => CompoundOp(vec![remop, suppop]),
-////                     _            => remop,
-////                 }
-////             },
-////             _ => remop,
-////         }
-////     }
-////
-////     /// Replace the subtree rooted in the first node with a subtree rooted
-////     /// in the second node
-////     pub fn replace(&mut self, from: Node, to: Node) -> TreeOp {
-////         let left   = self.left(from);
-////         let right  = self.right(from);
-////         let parent = self.parent(from);
-////
-////         self.set_parent(to, parent);
-////         if parent == None {
-////             self.root = to;
-////         }
-////
-////         self.set_left(to, left);
-////         if let Some(l) = left {
-////             self.set_right(l, Some(to));
-////         } else if Some(p) = parent {
-////             self.set_first_child(p, Some(to));
-////         }
-////
-////         self.set_right(to, right);
-////         if let Some(r) = right {
-////             self.set_left(r, Some(to));
-////         }
-////
-////         ReplaceOp(from, left, right, parent)
-////     }
-////
-////     /// Split the parent edge of the given node
-////     pub fn split(&mut self, node: Node) -> (Node, TreeOp) {
-////         let new_parent =
-////         match self.parent(node) {
-////         }
-////         SplitOp(node)
-////     }
-////
-////     /// Contract the parent edge of the given node
-////     pub fn contract(&mut self, node: Node) -> TreeOp {
-////     }
-////
-////     /// Resolve the given subset of nodes.  They must all have the same
-////     /// parent and must be given in the same order as they appear as
-////     /// children of the parent.
-////     pub fn resolve(&mut self, nodes: &[Node]) -> (Option<Node>, TreeOp) {
-////     }
-////
-////     /// The parent of the given node
-////     pub fn parent(&self, node: Node) -> Option<Node> {
-////     }
-////
-////     /// The left sibling of the given node
-////     pub fn left(&self, node: Node) -> Option<Node> {
-////     }
-////
-////     /// The right sibling of the given node
-////     pub fn right(&self, node: Node) -> Option<Node> {
-////     }
-////
-////     /// The first child of the given node
-////     pub fn first_child(&self, node: Node) -> Option<Node> {
-////     }
-////
-////     /// The label of this node, if it's a leaf
-////     pub fn label(&self, node: Node) -> Option<usize> {
-////     }
-////
-////     /// Is the current node a leaf?
-////     pub fn is_leaf(&self, node: Node) -> bool {
-////     }
-////
-////     /// Is the current node an internal node?
-////     pub fn is_internal(&self, node: Node) -> bool {
-////     }
-////
-////     /// Is the current node the root?
-////     pub fn is_root(&self, node: Node) -> bool {
-////     }
-////
-////     /// Is the current node the leftmost child of its parent?
-////     pub fn is_left(&self, node: Node) -> bool {
-////     }
-////
-////     /// Is the current node the rightmost child of its parent?
-////     pub fn is_right(&self, node: Node) -> bool {
-////     }
-//}
 
-///// Builder to construct a tree
-//pub struct TreeBuilder {
-//
-//    /// The set of leaf labels
-//    leaf_labels: Vec<String>,
-//
-//    /// Map from leaf label back to its ID (needed to ensure that
-//    /// leaves with the same label but in different trees get the
-//    /// same ID).
-//    leaf_ids: HashMap<String, usize>,
-//
-//    /// The nodes for the current tree
-//    current_tree: Option<Tree>,
-//
-//    /// The trees built so far
-//    trees: Vec<Tree>,
-//}
+/// An iterator over the children of a node
+pub struct ChildIter<'a, T: 'a> {
+    tree:  &'a Tree<T>,
+    child: Option<Node>,
+}
 
-//impl TreeBuilder {
-//
-//    pub fn new() -> TreeBuilder {
-//        TreeBuilder {
-//            leaf_labels:  Vec::new(),
-//            leaf_ids:     HashMap::new(),
-//            current_tree: None,
-//            trees:        Vec::new(),
-//        }
-//    }
-//}
-//
-//impl newick::TreeBuilder for TreeBuilder {
-//
-//    type Node = Node;
-//
-//    fn new_tree(&mut self) {
-//        self.current_tree = Some(Tree::new());
-//    }
-//
-//    fn new_leaf(&mut self, label: String) -> Self::Node {
-//        let leaf_id = *self.leaf_ids.entry(label.clone()).or_insert({
-//            let leaf_id = self.leaf_labels.len();
-//            self.leaf_labels.push(label);
-//            leaf_id
-//        });
-//        self.current_tree.as_mut().unwrap().new_leaf(leaf_id)
-//    }
-//
-//    fn new_node(&mut self, children: Vec<Self::Node>) -> Self::Node {
-//        self.current_tree.as_mut().unwrap().new_internal(children)
-//    }
-//
-//    fn finish_tree(&mut self, root: Self::Node) {
-//        self.current_tree.as_mut().unwrap().set_root(root);
-//        self.trees.push(self.current_tree.take().unwrap());
-//    }
-//}
+
+impl<'a, T: 'a> Iterator for ChildIter<'a, T> {
+
+    type Item = Node;
+
+    fn next(&mut self) -> Option<Node> {
+        self.child.map(|node| {
+            self.child = self.tree[node].right();
+            node
+        })
+    }
+}
