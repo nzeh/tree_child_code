9f969b13a563608bd63c15495c34e8b4699dad90
Norbert Zeh <nzeh@cs.dal.ca>
2018-05-27 10:35:41 -0300

    Cluster partition passes all tests

--- tree_child/src/clusters.rs
+++ tree_child/src/clusters.rs
@@ -11,12 +11,13 @@ use tree::traits::{Id, Tree, TreeBuilder};
 
 
 /// The type used to represent clusters in the cluster partition
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 pub struct Cluster(usize);
 
 
 /// Type used to distinguish between leaves that are original leaves and leaves that represent
 /// clusters produced by the cluster partition.
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub enum LoC<T> {
 
     /// An original leaf
@@ -174,7 +175,7 @@ impl<'t, Label, T> LcaMapper<'t, Label, T>
         // Return the initial mapper state
         LcaMapper {
             tree:      t1.0,
-            parents:   t2.0.nodes().map(|node| t1.0.parent(node)).collect(),
+            parents:   t2.0.nodes().map(|node| t2.0.parent(node)).collect(),
             intervals: &t2.1,
             mapping,
         }
@@ -238,7 +239,11 @@ fn identify_clusters<'t, Label, T, TNode>(
           Label:  't,
           TNode:  Copy + Id + PartialEq {
 
-    let mut cluster_nodes = vec![vec![None; forward_maps[0].len()]; forward_maps.len() + 1];
+    let mut cluster_nodes = vec![];
+    cluster_nodes.push(vec![None; forward_maps[0].len()]);
+    for m in backward_maps.iter() {
+        cluster_nodes.push(vec![None; m.len()]);
+    }
     let mut cluster_id = 0;
     'l: for node in 0..forward_maps[0].len() {
         if tree.is_leaf(TNode::new(node)) {
@@ -300,7 +305,7 @@ impl<Label, B, C, CNode> Decomposer<Label, B, C, CNode>
     /// Construct a new decomposer capable of building num_clusters + 1 clusters
     fn new(num_clusters: usize) -> Self {
         let mut builders = vec![];
-        for _ in 0..num_clusters + 1 {
+        for _ in 0..num_clusters {
             builders.push(B::new());
         }
         Decomposer { builders, phantom: PhantomData }
@@ -410,3 +415,218 @@ impl<Label, B, C, CNode> Decomposer<Label, B, C, CNode>
 //
 //    combined_seq
 //}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use tree::{Node, Tree, TreeBuilder};
+    use tree::traits::Tree as TreeTrait;
+    use tree::traits::TreeBuilder as TreeBuilderTrait;
+    use newick;
+
+    /// Construct a forest to be used in subsequent tests
+    fn build_forest() -> Vec<Tree<String>> {
+        let mut builder  = TreeBuilder::<String>::new();
+        let tree1_newick = "((a,((d,e),(((o,n),(l,m,p)),(f,(q,(r,s))),g))),(b,((h,i),(j,k)),c));";
+        let tree2_newick = "((b,(k,(i,(j,h)))),(a,(d,(e,(((l,((m,n),(o,p))),(r,(s,q)),g),f))),c));";
+        let tree3_newick = "((b,(k,(i,j),h)),((a,c),(((l,((o,n,p),m)),(f,(s,(q,r))),d),(e,g))));";
+        let mut newick   = String::new();
+        newick += tree1_newick;
+        newick.push('\n');
+        newick += tree2_newick;
+        newick.push('\n');
+        newick += tree3_newick;
+        newick.push('\n');
+        newick::parse_forest(&mut builder, &newick).unwrap();
+        builder.trees()
+    }
+
+    /// Test that the leaf intervals are computed correctly
+    #[test]
+    fn test_leaf_intervals() {
+        let trees                               = build_forest();
+        let intervals: Vec<Vec<(usize, usize)>> = trees.iter().map(leaf_intervals).collect();
+        assert_eq!(intervals[0],
+                   vec![
+                   (0,0),(1,1),(2,2),(1,2),(3,3),(4,4),(3,4),(5,5),(6,6),(7,7),(5,7),
+                   (3,7),(8,8),(9,9),(10,10),(11,11),(10,11),(9,11),(8,11),(12,12),
+                   (3,12),(1,12),(0,12),(13,13),(14,14),(15,15),(14,15),(16,16),(17,17),
+                   (16,17),(14,17),(18,18),(13,18),(0,18)]);
+        assert_eq!(intervals[1],
+                   vec![
+                   (0,0),(1,1),(2,2),(3,3),(4,4),(3,4),(2,4),(1,4),(0,4),
+                   (5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(9,10),(11,11),(12,12),(11,12),
+                   (9,12),(8,12),(13,13),(14,14),(15,15),(14,15),(13,15),(16,16),(8,16),
+                   (17,17),(8,17),(7,17),(6,17),(18,18),(5,18),(0,18)]);
+        assert_eq!(intervals[2],
+                   vec![
+                   (0,0),(1,1),(2,2),(3,3),(2,3),(4,4),(1,4),(0,4),
+                   (5,5),(6,6),(5,6),
+                   (7,7),(8,8),(9,9),(10,10),(8,10),(11,11),(8,11),(7,11),
+                   (12,12),(13,13),(14,14),(15,15),(14,15),(13,15),(12,15),(16,16),(7,16),
+                   (17,17),(18,18),(17,18),(7,18),(5,18),(0,18)]);
+    }
+
+    /// Test that the LCA mapping is correct
+    #[test]
+    fn test_lca_mapping() {
+        let trees     = build_forest();
+        let intervals = trees.iter().map(leaf_intervals);
+        let numbered_trees: Vec<(&Tree<String>, Vec<(usize, usize)>)>
+            = trees.iter().zip(intervals).collect();
+        let forward_maps: Vec<Vec<usize>> = numbered_trees[1..].iter()
+            .map(|t| map_to_lca(&numbered_trees[0], t))
+            .map(|m| m.into_iter().map(|node| node.id()).collect())
+            .collect();
+        let backward_maps: Vec<Vec<usize>> = numbered_trees[1..].iter()
+            .map(|t| map_to_lca(t, &numbered_trees[0]))
+            .map(|m| m.into_iter().map(|node| node.id()).collect())
+            .collect();
+        assert_eq!(forward_maps[0],
+                   vec![
+                   9,10,11,31,16,14,19,12,13,17,20,20,28,23,21,22,25,25,29,26,29,
+                   31,33,0,4,2,6,3,1,7,7,32,34,34]);
+        assert_eq!(forward_maps[1],
+                   vec![
+                   8,26,28,31,12,13,15,11,16,14,18,18,19,21,22,20,24,24,25,29,
+                   31,31,32,0,5,2,6,3,1,6,6,9,33,33]);
+        assert_eq!(backward_maps[0],
+                   vec![
+                   23,28,25,27,24,30,30,30,32,0,1,2,7,8,5,11,4,9,11,11,11,14,15,13,17,17,
+                   19,20,12,20,21,21,31,33,33]);
+        assert_eq!(backward_maps[1],
+                   vec![
+                   23,28,25,27,30,24,30,32,0,31,33,7,4,5,9,11,8,11,11,12,15,13,14,17,17,18,
+                   1,21,2,19,21,21,33,33]);
+    }
+
+    /// Test that the clusters are identified correctly
+    #[test]
+    fn test_identify_clusters() {
+        let trees = build_forest();
+        let (num_clusters, cluster_nodes) = {
+            let intervals = trees.iter().map(leaf_intervals);
+            let numbered_trees: Vec<(&Tree<String>, Vec<(usize, usize)>)>
+                = trees.iter().zip(intervals).collect();
+            let forward_maps = numbered_trees[1..].iter()
+                .map(|t| map_to_lca(&numbered_trees[0], t)).collect();
+            let backward_maps = numbered_trees[1..].iter()
+                .map(|t| map_to_lca(t, &numbered_trees[0])).collect();
+            identify_clusters(&trees[0], forward_maps, backward_maps)
+        };
+        assert_eq!(num_clusters, 5);
+        assert_eq!(cluster_nodes[0],
+                   vec![
+                   None, None, None, None, None, None, None, None, None, None, None,
+                   Some(0), None, None, None, None, None, Some(1), None, None, None,
+                   Some(2), None, None, None, None, None, None, None, None, Some(3),
+                   None, None, Some(4)]);
+        assert_eq!(cluster_nodes[1],
+                   vec![
+                   None, None, None, None, None, None, None, Some(3), None,
+                   None, None, None, None, None, None, None, None, None, None, None, Some(0),
+                   None, None, None, None, Some(1), None, None, None, None, None, Some(2),
+                   None, None, Some(4)]);
+        assert_eq!(cluster_nodes[2],
+                   vec![
+                   None, None, None, None, None, None, Some(3), None, None, None, None,
+                   None, None, None, None, None, None, None, Some(0), None, None, None,
+                   None, None, Some(1), None, None, None, None, None, None, Some(2),
+                   None, Some(4)]);
+    }
+
+    /// Translate the labels of the trees in a cluster partition into string labels so they
+    /// can be sent to format_forest
+    fn with_string_labels(clusters: Vec<Vec<Tree<LoC<String>>>>) -> Vec<Vec<Tree<String>>> {
+
+        fn translate_labels(
+            builder: &mut TreeBuilder<String>,
+            tree: &Tree<LoC<String>>,
+            node: Node) -> Node {
+
+            match tree.label(node) {
+
+                Some(label) => {
+                    match label {
+                        LoC::Leaf(label)         => builder.new_leaf(label.clone()),
+                        LoC::Cluster(Cluster(c)) => builder.new_leaf(format!("C{}", c)),
+                    }
+                },
+
+                None => {
+                    let children = tree.children(node).map(
+                        |child| translate_labels(builder, tree, child)).collect();
+                    builder.new_node(children)
+                },
+            }
+        }
+
+        let mut output_clusters = vec![];
+        for cluster in clusters {
+            let mut builder = TreeBuilder::<String>::new();
+            for tree in cluster {
+                builder.new_tree();
+                let root = translate_labels(&mut builder, &tree, tree.root().unwrap());
+                builder.finish_tree(root);
+            }
+            output_clusters.push(builder.trees());
+        }
+        output_clusters
+    }
+
+    /// Test that the trees are decomposed correctly
+    #[test]
+    fn test_decompose_trees() {
+        let trees = build_forest();
+        let (num_clusters, cluster_nodes) = {
+            let intervals = trees.iter().map(leaf_intervals);
+            let numbered_trees: Vec<(&Tree<String>, Vec<(usize, usize)>)>
+                = trees.iter().zip(intervals).collect();
+            let forward_maps = numbered_trees[1..].iter()
+                .map(|t| map_to_lca(&numbered_trees[0], t)).collect();
+            let backward_maps = numbered_trees[1..].iter()
+                .map(|t| map_to_lca(t, &numbered_trees[0])).collect();
+            identify_clusters(&trees[0], forward_maps, backward_maps)
+        };
+        let clusters =
+            with_string_labels(decompose_trees::<
+                               String,
+                               Tree<String>, Node,
+                               Tree<LoC<String>>, Node,
+                               TreeBuilder<LoC<String>>>(trees, num_clusters, cluster_nodes));
+        assert_eq!(clusters.len(), 5);
+        let expected_newicks = [
+            "((o,n),(l,m,p));\n(l,((m,n),(o,p)));\n(l,((o,n,p),m));\n",
+            "(q,(r,s));\n(r,(s,q));\n(s,(q,r));\n",
+            "((d,e),(C0,(f,C1),g));\n(d,(e,((C0,C1,g),f)));\n((C0,(f,C1),d),(e,g));\n",
+            "((h,i),(j,k));\n(k,(i,(j,h)));\n(k,(i,j),h);\n",
+            "((a,C2),(b,C3,c));\n((b,C3),(a,C2,c));\n((b,C3),((a,c),C2));\n",
+        ];
+        for i in 0..clusters.len() {
+            assert_eq!(newick::format_forest(&clusters[i]).unwrap(), expected_newicks[i]);
+        }
+    }
+
+    /// Now test the whole package
+    #[test]
+    fn test_partition() {
+        let trees = build_forest();
+        let clusters = with_string_labels(partition::<
+                                          String,
+                                          Tree<String>, Node,
+                                          Tree<LoC<String>>, Node,
+                                          TreeBuilder<LoC<String>>>(trees));
+        assert_eq!(clusters.len(), 5);
+        let expected_newicks = [
+            "((o,n),(l,m,p));\n(l,((m,n),(o,p)));\n(l,((o,n,p),m));\n",
+            "(q,(r,s));\n(r,(s,q));\n(s,(q,r));\n",
+            "((d,e),(C0,(f,C1),g));\n(d,(e,((C0,C1,g),f)));\n((C0,(f,C1),d),(e,g));\n",
+            "((h,i),(j,k));\n(k,(i,(j,h)));\n(k,(i,j),h);\n",
+            "((a,C2),(b,C3,c));\n((b,C3),(a,C2,c));\n((b,C3),((a,c),C2));\n",
+        ];
+        for i in 0..clusters.len() {
+            assert_eq!(newick::format_forest(&clusters[i]).unwrap(), expected_newicks[i]);
+        }
+    }
+}
